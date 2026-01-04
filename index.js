a0c34dc94087c4069d3e5e03b248591b514edaa9
Nathan Gifford <nathan@growthmarketingfirm.com>
2026-01-04 01:27:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const TOKEN = 'pat-na1-718c969c-3419-4d0e-a377-462d5269ef96'; 
+const PRIVATE_APP_ACCESS = `pat-na1-718c969c-3419-4d0e-a377-462d5269ef96`;
 
-async function test() {
-    try {
-        console.log('Testing token on Contacts endpoint...');
-        const res = await axios.get('https://api.hubapi.com/crm/v3/objects/contacts?limit=1', {
-            headers: { 'Authorization': `Bearer ${TOKEN}` }
-        });
-        console.log('SUCCESS! Token is valid.');
-    } catch (e) {
-        console.log('FAILED:', e.response ? e.response.data : e.message);
+app.get('/', async (req, res) => {
+    const companies = 'https://api.hubspot.com/crm/v3/objects/contacts';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
     }
-}
-
-test();
+    try {
+        const response = await axios.get(companies, { headers });
+        res.json(response.data.results);    
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN ? process.env.ACCESS_TOKEN.trim() : '';
 
